5f6a73a847e6b506c0c1867f0d66f0853f6a9167
Darren Brain <darren.brain@gmail.com>
2024-02-16 17:11:18 -0500

    Add User, List, and Item typedefs to schema

--- server/schemas/typeDef.js
+++ server/schemas/typeDef.js
@@ -1,14 +1,37 @@
 const typeDefs = `
 type User {
-	_id: ID
+	_id: ID!
 	username: String!
 	password: String!
 	firstName: String!
 	lastName: String!
+	email: String!
+	lists: [List]
 }
 
+type List {
+	_id: ID!,
+	owner: String!
+	accessCode: String!,
+	listType: String!,
+	listName: String!,
+	eventDate: date,
+	items: [Item]
+}
+
+type Item {
+	_id: ID!,
+	name: String!,
+	link: String,
+	quantity: Integer!,
+	quantityBought: Integer!,
+	note: String
+} 
+
+
 type Query {
-	users: [User]
+	user(_id: ID!): User
+	list(_id: ID!): List
 }
 `
 
