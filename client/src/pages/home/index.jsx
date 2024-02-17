37cafba0a40f71ea6ca13031998e16ca9888356c
chris <chrisj21283@gmail.com>
2024-02-17 13:19:18 -0500

    added homepage

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,22 +1,22 @@
-import { UPDATE_THEME } from '../../utils/actions';
-import { useStoreContext } from '../../utils/GlobalStore';
-
+// import { UPDATE_THEME } from '../../utils/actions';
+// import { useStoreContext } from '../../utils/GlobalStore';
+import './assets/home.css'
 const Home = () => {
-	const [state, dispatch] = useStoreContext();
-
-	function changeTheme() {
-		dispatch({
-			type: UPDATE_THEME
-		});
-	}
+	// const [state, dispatch] = useStoreContext();
 
 	return (
 		<>
-			<p>Dark Mode: {state.darkMode.toString()}</p>
-			<button onClick={changeTheme}>Change</button>
-			<h1>Hello World</h1>
+			<div className="hero">
+				<div className="overlay"></div>
+				<div className="content">
+					<h1>Welcome to Our Event Registry</h1>
+					<p>Make your special occasions memorable with our registry service.<br />Whether it's a wedding, baby shower, or dinner event, find and manage your gifts hassle-free!</p>
+					<a href="#" className="btn">Get Started</a>
+				</div>
+			</div>
 		</>
 	);
+
 };
 
 export default Home;
